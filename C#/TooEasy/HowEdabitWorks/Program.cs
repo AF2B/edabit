a9b59f92a06c420e5c7755d41bfa669b09753108
andreborba <dev_andre.filipe@outlook.com>
2022-12-09 19:38:14 -0300

    C#: How Edabit Works

---
+++ C#/TooEasy/HowEdabitWorks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HowEdabitWorks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Edabit!");
+        }
+    }
+}
+
+namespace HowEdabitWorks.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("Hello, Edabit!", Program.Main(null));
+        }
+    }
+}
