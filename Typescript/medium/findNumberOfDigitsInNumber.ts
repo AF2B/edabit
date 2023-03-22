ce3bda7dff1acedcf40c438220f341cebaca20bd
andre.borba <dev_andre.filipe@outlook.com>
2023-03-22 03:12:25 -0300

    Typescript: find number of digits resolved

---
+++ Typescript/medium/findNumberOfDigitsInNumber.ts
@@ -0,0 +1,6 @@
+const numOfDigits = (num: number): number => num.toString().length;
+
+console.log(numOfDigits(12345)); // 5
+console.log(numOfDigits(999)); // 3
+console.log(numOfDigits(100000)); // 6
+
