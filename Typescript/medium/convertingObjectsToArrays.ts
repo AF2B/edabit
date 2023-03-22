c4daba8cf662ca26125d3ea147597dff401ea83b
andre.borba <dev_andre.filipe@outlook.com>
2023-03-22 03:26:11 -0300

    Typescript: completing the note question

--- Typescript/medium/convertingObjectsToArrays.ts
+++ Typescript/medium/convertingObjectsToArrays.ts
@@ -1,4 +1,8 @@
-const toArray = (obj: Object) => Object.entries(obj);
+const toArray = (obj: Object) => {
+  if(obj === null) return [];
+
+  return Object.entries(obj);
+}
 
 console.log(toArray({ a: 1, b: 2 })); // [['a', 1], ['b', 2]]
 console.log(toArray({ shrimp: 15, tots: 12 })); // [['shrimp', 15], ['tots', 12]]
