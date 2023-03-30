745b3a43ff350132776c48e2aaf4813738fa213f
andre.borba <dev_andre.filipe@outlook.com>
2023-03-30 13:17:02 -0300

    Typescript: refactored test variable

--- Typescript/medium/howMuchIsTrue.ts
+++ Typescript/medium/howMuchIsTrue.ts
@@ -1,6 +1,8 @@
 const countTrue = (arr: boolean[]): number => arr.filter((x) => x).length;
 
-const testOne = countTrue([true, false, false, true, false]); // 2
+const arrTest: boolean[] = [true, false, false, true, false]
+
+const testOne = countTrue(arrTest); // 2
 const testTwo = countTrue([]); // 0
 
 console.log(testOne);
